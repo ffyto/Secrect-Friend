9c7feca3e470f1d300159f994fd9f39902137aef
Mauricio <cmauricio.arango@gmail.com>
2023-01-12 11:30:14 -0500

    adds validation rules

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,11 @@
 class Location < ApplicationRecord
   has_many :workers
+  validates :name, uniqueness: {
+    message: "El nombre del departamento ya ha sido usado"
+    },
+    length: {
+    minimum: 3,
+    too_short: "El nombre debe tener una longitud mínima de 3 caracteres"
+    },
+    format: { with: /\A([A-Za-zñáéíóú0-9]+[\s]*)+\z/, message: "El nombre solo puede contener letras y números." }
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,3 +1,11 @@
 class Worker < ApplicationRecord
   belongs_to :location
+  validates :name, length: {
+    minimum: 3,
+    too_short: "El nombre debe tener una longitud mínima de 3 caracteres"
+  },
+  format: { with: /\A([A-Za-zñáéíóú]+[\s]*)+\z/, message: "El nombre solo puede contener letras." }
+  validates :location, presence: {
+    message: 'Debe ingresarse el id de un departamento para el trabajador.'
+  }
 end
